6f530105a7b2e67bb7ef5fe8eb09dd015ebe52c2
Saeid Mohadjer <saeid@fastmail.fm>
2024-01-08 17:33:10 +0100

    update

--- src/app/grid/ageEditor.ts
+++ src/app/grid/ageEditor.ts
@@ -6,23 +6,34 @@ export class AgeEditor {
   init(params: any) {
     this.eInput = document.createElement('input');
     this.eInput.classList.add('numeric-input');
-    this.eInput.addEventListener('keydown', this.validateEdit);
+    //this.eInput.addEventListener('keydown', this.validateEdit);
+    this.eInput.addEventListener('input', this.validateInput);
     console.log('init', params.value, typeof params.value);
     this.oldValue = params.value;
   }
 
-  validateEdit = (e: any) => {
-    if (e.key === 'Tab' || e.key === 'Enter' || e.key === 'Escape') {
-      if (!this.validateValue(this.getValue())) {
-        console.log('adding invalid class...');
-        this.eInput.classList.add('invalid');
-        e.preventDefault();
-      }
+  // validateEdit = (e: any) => {
+  //   if (e.key === 'Tab' || e.key === 'Enter' || e.key === 'Escape') {
+  //     if (!this.validateValue(this.getValue())) {
+  //       console.log('adding invalid class...');
+  //       this.eInput.classList.add('invalid');
+  //       e.preventDefault();
+  //     }
+  //   }
+  // }
+
+  validateInput = (e: any) => {
+    const value = e.target.value;
+    if (isNaN(value) || !this.validateValue(value)) {
+      this.eInput.classList.add('invalid');
+      e.preventDefault();
+    } else {
+      this.eInput.classList.remove('invalid');
     }
   }
 
   validateValue = (value: any) => {
-    return value <= 100;
+    return value <= 100 && value > 0;
   }
 
   // gets called once when grid ready to insert the element
@@ -44,13 +55,13 @@ export class AgeEditor {
 
   // returns the new value after editing
   getValue() {
-    console.log('getvalue', this.eInput.value, typeof this.eInput.value);
     return Number(this.eInput.value);
   }
 
   // any cleanup we need to be done here
   destroy() {
-    this.eInput.removeEventListener('keydown', this.validateEdit);
+    //this.eInput.removeEventListener('keydown', this.validateEdit);
+    this.eInput.removeEventListener('input', this.validateInput);
   }
 
   // if true, then this editor will appear in a popup
